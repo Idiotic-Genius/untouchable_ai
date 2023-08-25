186eac097e922ccf0a5d662757158aeade34f264
Idiotic-Genius <kimslor@hotmail.com>
2023-08-25 10:07:45 -0700

    added wasd keys for movement option

--- player.py
+++ player.py
@@ -18,13 +18,21 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         # Move the player left or right
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
+        if (
+            keys[pygame.K_LEFT] or keys[pygame.K_a]
+        ) and self.rect.left > 0:
             self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.rect.right < const.SCREEN_WIDTH:
+        if (
+            keys[pygame.K_RIGHT] or keys[pygame.K_d]
+        ) and self.rect.right < const.SCREEN_WIDTH:
             self.rect.x += self.speed
-        if keys[pygame.K_UP] and self.rect.top > 0:
+        if (
+            keys[pygame.K_UP] or keys[pygame.K_w]
+        ) and self.rect.top > 0:
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN] and self.rect.bottom < const.SCREEN_HEIGHT:
+        if (
+            keys[pygame.K_DOWN] or keys[pygame.K_s]
+        ) and self.rect.bottom < const.SCREEN_HEIGHT:
             self.rect.y += self.speed
 
     def increase_time(self, value: int) -> None:
